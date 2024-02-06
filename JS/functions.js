56ea2a0c0ce6e52a49f8e02adf8d47b6dbfbe408
topiasraappana <topias.raappana@gmail.com>
2024-02-06 13:16:56 +0200

    matikkapeli

---
+++ JS/functions.js
@@ -0,0 +1,32 @@
+'use strict'
+
+let rand_num1 = 0
+let rand_num2 = 0
+
+const getRandomInNumberInRange = (min,max) => {
+    return Math.floor(Math.random() * max) + min;
+}
+
+const randomizeNumbers = () => {
+    rand_num1 = getRandomInNumberInRange(1,10)
+    rand_num2 = getRandomInNumberInRange(1,10)
+    document.querySelector('#num1').innerHTML = rand_num1
+    document.querySelector('#num2').innerHTML = rand_num2
+}
+
+addEventListener("DOMContentLoaded", () => {
+    randomizeNumbers()
+});
+
+document.querySelector('button').addEventListener('click',() => {
+    const answer = Number(document.querySelector('input').value)
+    const correctAnswer = rand_num1 + rand_num2
+    if (answer === correctAnswer){
+        alert('Correct!')
+    } else{
+        alert('Incorrect!')
+    }
+
+    randomizeNumbers()
+    document.querySelector('input').value=''
+})
